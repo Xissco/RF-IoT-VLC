9e47c2b171aabbad80d9756b5e85ac3dc9604976
FranCisco <52544926+Xissco@users.noreply.github.com>
2021-04-18 11:41:25 -0500

    Traduccion de mensajes y programacion de contador

--- Arduino/sketch_apr17b/sketch_apr17b.ino
+++ Arduino/sketch_apr17b/sketch_apr17b.ino
@@ -1,28 +1,74 @@
-#include <Ethernet.h>
 #include <SPI.h>
+#include <Ethernet.h>
 
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x10  //Sustituir YY por el numero de KIT de Arduino
-};
-
-void setup() {
+byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
+int cont = 0;
+void setup() 
+{  
+  Ethernet.init(10);
   Serial.begin(9600);
-  // start the Ethernet connection:
-  Serial.println("Inicializando red...");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    for (;;)
-      ;
+  while (!Serial) {;}
+  
+  Serial.println("Initialize Ethernet with DHCP:");
+  
+  while (Ethernet.begin(mac) == 0) 
+  {
+    Serial.println("Fallo al configurar DHCP usando Ethernet.");
+    
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) 
+    {
+      Serial.println("No se encontro Ethernet Shield.");
+    }
+    else if (Ethernet.linkStatus() == LinkOFF) 
+    {
+      Serial.println("Cable Ethernet no conectado.");
+    }
+    delay(5000);
   }
-  else {
-    Serial.print("IP asignada por DHCP: ");
-    Serial.println(Ethernet.localIP());
-  }
+  Serial.print("Conexion Ethernet exitosa, Direccion IP: ");
+  Serial.println(Ethernet.localIP());
 }
 
-void loop() {
-  Serial.print("Para comprobar que funciona haz ping a la IP: " + Ethernet.localIP());
+void loop() 
+{
+  renovarDHCP();
+  cont++;
+  if (cont == 101) cont = 0;
+  Serial.println(cont);
+  delay(1000);  
+}
 
-  for (;;)
-    ;
+void renovarDHCP()
+{
+  switch (Ethernet.maintain()) {
+    case 1:
+      //renewed fail
+      Serial.println("Error en la renovación");
+      break;
+
+    case 2:
+      //renewed success
+      Serial.println("renovacion exitosa");
+      //print your local IP address:
+      Serial.print("La direccion IP es: ");
+      Serial.println(Ethernet.localIP());
+      break;
+
+    case 3:
+      //rebind fail
+      Serial.println("Error: Revinculacion Fallida");
+      break;
+
+    case 4:
+      //rebind success
+      Serial.println("Revinculacion exitosa");
+      //print your local IP address:
+      Serial.print("la direccion IP es: ");
+      Serial.println(Ethernet.localIP());
+      break;
+
+    default:
+      //nothing happened
+      break;
+  }  
 }
