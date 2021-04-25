85b1b586f2b7f5e23edc383e437f5e457fad6198
FranCisco <52544926+Xissco@users.noreply.github.com>
2021-04-24 20:30:59 -0500

    Establecido monitoreo con Thingsboard

--- Arduino/Infraestructura/Infraestructura.ino
+++ Arduino/Infraestructura/Infraestructura.ino
@@ -1,14 +1,32 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
+#include <PubSubClient.h>   
+#include <ArduinoJson.h>   
+
+EthernetClient ethernetClient;
+PubSubClient mqttClient(ethernetClient);
+EthernetUDP Udp;
 
 byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x03}; 
 char IPsemaforo[] = "172.16.1.253"; 
 int puertoSemaforo = 8001;
 unsigned int localPort = 8002;
 byte emergencyFlag = 0;
+char JSON_Data_Tx[100];
+unsigned long previousMillis = 0;
+long interval = 1*1000; 
 
-EthernetUDP Udp;
+// ****************************** ThingsBoard login details ******************************
+const char* server = "172.16.1.254";           // MQTT Broker (i.e. server)
+const int port = 1883;                         // Default MQTT port
+ 
+const char* client_id = "Infraestructura 1";   // Can be anything
+const char* username =  "kuswzx9USm0ufFMXBMSm";   // Authentication token here
+
+const char* topicToPublish_DATA = "v1/devices/me/telemetry"; // Topic address to publish to for sending data. 
+const char* topicToPublish_ATTRIBUTES = "v1/devices/me/attributes"; // Topic address to publish to for sending attributes. 
+// ****************************************************************************************
 
 void setup()
 {
@@ -30,17 +48,68 @@ void setup()
     }
     delay(5000);
   }
-  
-  Udp.begin(localPort);
 
   Serial.print("My IP Address: ");
   Serial.println(Ethernet.localIP());
+  Udp.begin(localPort);
+  mqttClient.setServer(server, port);
 }
 
 void loop()
 {
-  UDPSendPacket (emergencyFlag, IPsemaforo, puertoSemaforo);
-  delay(1000);
+  unsigned long currentMillis = millis();
+  renovarDHCP();
+  
+  if (!mqttClient.connected())
+  {
+    if (mqttClient.connect(client_id, username, NULL)) Serial.println("Cliente mqtt no conectado");
+  }
+  
+  if(currentMillis - previousMillis > interval) 
+  {
+    previousMillis = currentMillis; // reset the previous millis, so that it will continue to publish data.
+    UDPSendPacket (emergencyFlag, IPsemaforo, puertoSemaforo);
+    create_JSON_Data_Tx(); // Set up the data to be published
+    mqttClient.publish(topicToPublish_ATTRIBUTES, JSON_Data_Tx);
+    if(emergencyFlag == 0) emergencyFlag = 1;
+    else emergencyFlag = 0;
+  }
+  mqttClient.loop();
+}
+
+void renovarDHCP()
+{
+  switch (Ethernet.maintain()) {
+    case 1:
+      //renewed fail
+      Serial.println("Error en la renovación");
+      break;
+
+    case 2:
+      //renewed success
+      Serial.println("renovacion exitosa");
+      //print your local IP address:
+      Serial.print("La direccion IP es: ");
+      Serial.println(Ethernet.localIP());
+      break;
+
+    case 3:
+      //rebind fail
+      Serial.println("Error: Revinculacion Fallida");
+      break;
+
+    case 4:
+      //rebind success
+      Serial.println("Revinculacion exitosa");
+      //print your local IP address:
+      Serial.print("la direccion IP es: ");
+      Serial.println(Ethernet.localIP());
+      break;
+
+    default:
+      //nothing happened
+      break;
+  }  
 }
 
 void UDPSendPacket(byte data, char remote_IP[], int remote_port)
@@ -48,4 +117,15 @@ void UDPSendPacket(byte data, char remote_IP[], int remote_port)
   Udp.beginPacket(remote_IP, remote_port);
   Udp.write(data);
   Udp.endPacket();    
+}
+
+void create_JSON_Data_Tx(void)
+{
+  StaticJsonBuffer<200> JSON_Buffer; // Allocate JSON buffer with 200-byte pool
+  JsonObject& JSON_Object = JSON_Buffer.createObject(); // Create JSON object (i.e. document)
+  
+  // Now populate the JSON document with data
+  JSON_Object["state"] = emergencyFlag; // Create JSON object named "Temperature", assigned with our temperature data
+  
+  JSON_Object.printTo(JSON_Data_Tx); // Store the data on global variable
 }
--- Arduino/Semaforo/Semaforo.ino
+++ Arduino/Semaforo/Semaforo.ino
@@ -1,11 +1,31 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
+#include <PubSubClient.h>   
+#include <ArduinoJson.h>   
+
+EthernetClient ethernetClient;
+PubSubClient mqttClient(ethernetClient);
+EthernetUDP Udp;
 
 byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02}; 
 unsigned int localPort = 8001;
+int packetSize;
+byte emergencyFlag = 0;
+char JSON_Data_Tx[100];
+unsigned long previousMillis = 0;
+long interval = 1*1000; 
 
-EthernetUDP Udp;
+// ****************************** ThingsBoard login details ******************************
+const char* server = "172.16.1.254";           // MQTT Broker (i.e. server)
+const int port = 1883;                         // Default MQTT port
+ 
+const char* client_id = "Infraestructura 1";   // Can be anything
+const char* username =  "LodxRSnCktZNrFKLhKm7";   // Authentication token here
+
+const char* topicToPublish_DATA = "v1/devices/me/telemetry"; // Topic address to publish to for sending data. 
+const char* topicToPublish_ATTRIBUTES = "v1/devices/me/attributes"; // Topic address to publish to for sending attributes. 
+// ****************************************************************************************
 
 void setup()
 {
@@ -27,26 +47,88 @@ void setup()
     }
     delay(5000);
   }
-  
-  Udp.begin(localPort);
 
   Serial.print("My IP Address: ");
   Serial.println(Ethernet.localIP());
+  Udp.begin(localPort);
+  mqttClient.setServer(server, port);
 }
 
 void loop()
 {
-  int packetSize = Udp.parsePacket();
-  if (packetSize)
+  unsigned long currentMillis = millis();
+  renovarDHCP();
+
+  if (!mqttClient.connected())
   {
-    byte data2 = UDPReceivePacket();
-    Serial.println(data2);
+    if (mqttClient.connect(client_id, username, NULL)) Serial.println("Cliente mqtt no conectado");
   }
-}
-  byte UDPReceivePacket()
+  if(currentMillis - previousMillis > interval) 
   {
+    previousMillis = currentMillis; // reset the previous millis, so that it will continue to publish data.
+    packetSize = Udp.parsePacket();
+    if (packetSize)
+    {
+      emergencyFlag = UDPReceivePacket();
+      Serial.println(emergencyFlag);
+    }
+    create_JSON_Data_Tx(); // Set up the data to be published
+    mqttClient.publish(topicToPublish_ATTRIBUTES, JSON_Data_Tx);
+ }
+  mqttClient.loop();    
+}
+
+void renovarDHCP()
+{
+  switch (Ethernet.maintain()) {
+    case 1:
+      //renewed fail
+      Serial.println("Error en la renovación");
+      break;
+
+    case 2:
+      //renewed success
+      Serial.println("renovacion exitosa");
+      //print your local IP address:
+      Serial.print("La direccion IP es: ");
+      Serial.println(Ethernet.localIP());
+      break;
+
+    case 3:
+      //rebind fail
+      Serial.println("Error: Revinculacion Fallida");
+      break;
+
+    case 4:
+      //rebind success
+      Serial.println("Revinculacion exitosa");
+      //print your local IP address:
+      Serial.print("la direccion IP es: ");
+      Serial.println(Ethernet.localIP());
+      break;
+
+    default:
+      //nothing happened
+      break;
+  }  
+}
+
+byte UDPReceivePacket()
+{
     byte bufferRead [1]; 
     Udp.read(bufferRead,1);
     byte data = bufferRead[0];
     return data;    
-  }
+}
+
+void create_JSON_Data_Tx(void)
+{
+  StaticJsonBuffer<200> JSON_Buffer; // Allocate JSON buffer with 200-byte pool
+  JsonObject& JSON_Object = JSON_Buffer.createObject(); // Create JSON object (i.e. document)
+  
+  // Now populate the JSON document with data
+  JSON_Object["state"] = emergencyFlag; // Create JSON object named "Temperature", assigned with our temperature data
+  
+  JSON_Object.printTo(JSON_Data_Tx); // Store the data on global variable
+}
+  
